6aa1a417cdd63d71a3fcd0d7d6969caef536b922
apeeyush <peeyushagarwal1994@gmail.com>
2015-06-14 02:23:24 +0530

    Working code V1

--- cppCode/main.cpp
+++ cppCode/main.cpp
@@ -1,15 +1,19 @@
-#include<bits/stdc++.h>
+//
+//  main.cpp
+//  hackit
+//
+//  Created by Vansh Pahwa on 6/13/15.
+//  Copyright (c) 2015 Vansh Pahwa. All rights reserved.
+//
 
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-#include "mongoose.h"
-// #include <iostream>
-// #include <algorithm>
-// #include <math.h>
-// #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
 
 using namespace std;
+
+
 
 struct position
 {
@@ -68,7 +72,7 @@ public:
     state board;
     void boardInitialise(position my_loc,position opponent_loc,int boardSize)
     {
-        board.occupied = new vector<vector<bool> >(boardSize, vector<bool>(boardSize, false));
+        board.occupied = new std::vector<vector<bool> >(boardSize, vector<bool>(boardSize, false));
         board.my_loc=my_loc;
         board.opponent_loc=opponent_loc;
         board.setOccupied(my_loc);
@@ -110,7 +114,7 @@ position processor::myMove()
     board.setOccupied(next+board.my_loc);
     board.my_loc=next+board.my_loc;
     printBoard(board);
-    return board.my_loc;
+    return next;
 }
 
 
@@ -221,123 +225,37 @@ bool processor::isValidMove(position pos)
     }
     return true;
 }
-
-
-
-processor proc;
-
-
-
-static const char *s_no_cache_header =
-  "Cache-Control: max-age=0, post-check=0, "
-  "pre-check=0, no-store, no-cache, must-revalidate\r\n";
-
-// static void handle_restful_call(struct mg_connection *conn) {
-//   char n1[100], n2[100];
-//   mg_get_var(conn, "n1", n1, sizeof(n1));
-//   mg_get_var(conn, "n2", n2, sizeof(n2));
-
-//   mg_printf_data(conn, "{ \"result\": %lf }", strtod(n1, NULL) + strtod(n2, NULL));
-// }
-
-static void handle_ping_call(struct mg_connection *conn) {
-  mg_printf_data(conn, "{ \"ok\": \"true\" }");
-}
-
-position getPosition(char* s)
+int main(int argc, const char * argv[])
 {
-  std::vector<char*> v;
-  char* chars_array = strtok(s, "|");
-  while(chars_array)
-  {
-      v.push_back(chars_array);
-      chars_array = strtok(NULL, "|");
-  }
-  position ans;
-  ans.x = strtod(v[0], NULL)-1;
-  ans.y = strtod(v[1], NULL)-1;
-  return ans;
+    processor proc;
+    int msgtype;
+    int myx,myy,opx,opy,size;
+    position opp,my;
+    bool debug = true;
+    if(debug)
+    {
+        while(cin>>msgtype)
+        {
+            switch(msgtype)
+            {
+                case 0:
+                    cin>>myx>>myy>>opx>>opy>>size;
+                    my.x=myx; my.y=myy;
+                    opp.x=opx;opp.y=opy;
+                    proc.boardInitialise(my, opp, size);
+                    break;
+                case 1:
+                    int opx,opy;
+                    cin>>opx>>opy;
+                    opp.x=opx; opp.y=opy;
+                    proc.processOpponentMove(opp);
+                    break;
+                case 2:
+                    proc.myMove();
+                    break;
+            }
+        }
+    }
+    return 0;
 }
 
-static void handle_start_call(struct mg_connection *conn) {
-  char y[100], o[100], g[100];
-  mg_get_var(conn, "y", y, sizeof(y));
-  mg_get_var(conn, "o", o, sizeof(o));
-  mg_get_var(conn, "g", g, sizeof(g));
-
-  position myp = getPosition(y);
-  position opp = getPosition(o);
-  proc.boardInitialise(myp, opp, strtod(g,NULL));
-  // TODO : initGame(strtod(y, NULL), strtod(o, NULL), strtod(g, NULL))
-
-  mg_printf_data(conn, "{ \"ok\": \"true\" }");
-}
-
-static void handle_play_call(struct mg_connection *conn) {
-  char m[100];
-  mg_get_var(conn, "m", m, sizeof(m));
-  cout<<string(m)<<endl;
-  position opp = getPosition(m);
-  proc.processOpponentMove(opp);
-  position myMove = proc.myMove();
-  // TODO : Send move and ask ou
-  char s1[15];
-  sprintf(s1, "%d", myMove.x+1);
-  char s2[15];
-  sprintf(s2, "%d", myMove.y+1);
-  // string s1 = to_string(myMove.x+1);
-  // string s2 = to_string(myMove.y+1);
-  string s3 = "{ \"m\": \"" + string(s1) + "|" + string(s2) + "\" }";
-  // char * s3 = new char[100];
-  // s3 = strcat("{ \"m\": \"" , s1);
-  // s3 = strcat(s3, "|");
-  // s3 = strcat(s3, s2);
-  // s3 = strcat(s3, "\" }");
-  char const *pchar = s3.c_str(); 
-  mg_printf_data(conn, pchar);
-  cout<<s3<<endl;
-}
-
-
-static int ev_handler(struct mg_connection *conn, enum mg_event ev) {
-  switch (ev) {
-    case MG_AUTH: return MG_TRUE;
-    case MG_REQUEST:
-
-      if (!strcmp(conn->uri, "/ping")) {
-        handle_ping_call(conn);
-        return MG_TRUE;
-      }
-      if (!strcmp(conn->uri, "/start")) {
-        handle_start_call(conn);
-        return MG_TRUE;
-      }
-      if (!strcmp(conn->uri, "/play")) {
-        handle_play_call(conn);
-        return MG_TRUE;
-      }
-      return MG_FALSE;
-      // if (!strcmp(conn->uri, "/api/sum")) {
-      //   handle_restful_call(conn);
-      //   return MG_TRUE;
-      // }
-      // mg_send_file(conn, "index.html", s_no_cache_header);
-      // return MG_MORE;
-    default: return MG_FALSE;
-  }
-}
-
-int main(void) {
-  struct mg_server *server;
-  // Create and configure the server
-  server = mg_create_server(NULL, ev_handler);
-  mg_set_option(server, "listening_port", "8080");
-  // Serve request. Hit Ctrl-C to terminate the program
-  printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
-  for (;;) {
-    mg_poll_server(server, 1000);
-  }
-  // Cleanup, and free server instance
-  mg_destroy_server(&server);
-  return 0;
-}
